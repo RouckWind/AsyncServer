76a11b7a2bf9d1bc19c9269d8ac5c4925919be8d
RouckWind <infinite11145@gmail.com>
2023-06-04 20:49:40 +0200

    minor changes in files

--- gui/main.cpp
+++ gui/main.cpp
@@ -1,6 +1,5 @@
 #include "inc/mainwindow.h"
 
-#include <QWidget>
 #include <QApplication>
 
 int main(int argc, char *argv[]) {
--- gui/src/mainwindow.cpp
+++ gui/src/mainwindow.cpp
@@ -1,9 +1,3 @@
-//
-// Created by rouckwind on 04.06.23.
-//
-
-// You may need to build the project (run Qt uic code generator) to get "ui_MainWindow.h" resolved
-
 #include "../inc/mainwindow.h"
 #include "ui_mainwindow.h"
 
