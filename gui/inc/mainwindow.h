73828044b8dd2d25d3b7cafe323d226ca887ad2d
RouckWind <infinite11145@gmail.com>
2023-06-13 11:53:16 +0200

    change mainwindow class

--- gui/inc/mainwindow.h
+++ gui/inc/mainwindow.h
@@ -1,18 +1,32 @@
+#include <server.h>
+
 #include <QListWidget>
 #include <QMainWindow>
 #include <QPushButton>
 #include <QVBoxLayout>
+#include <QString>
 
 class MainWindow : public QMainWindow {
     Q_OBJECT
 
 public:
     explicit MainWindow(QWidget* parent = nullptr);
+    ~MainWindow() override;
 
 private slots:
 
+    void clientsCount();
+    void clientJoined();
+    void clientLeft();
+    void sendData();
+    void receiveData();
+    void clientConnected();
+
     void onpushButton_clicked();
 
-public:
-    ~MainWindow() override;
+private:
+    TCPServer::Server server;
+    QListWidget* listWidget;
+    QVBoxLayout* layout1;
+    QPushButton* pushButton;
 };
